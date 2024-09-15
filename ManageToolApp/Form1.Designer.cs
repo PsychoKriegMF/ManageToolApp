605554dc4816b19c112a6d172c51d86718ae47fd
Administrator <Administrator@DESKTOP-GR6DUC0>
2024-09-15 22:26:02 +0500

    Добавьте файлы проекта.

---
+++ ManageToolApp/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ManageToolApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtInventoryNumber = new System.Windows.Forms.TextBox();
+            this.txtShortName = new System.Windows.Forms.TextBox();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.txtProfessionalLevel = new System.Windows.Forms.TextBox();
+            this.txtWearPercentage = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(743, 524);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(777, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(777, 58);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Обновить";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(777, 104);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtInventoryNumber
+            // 
+            this.txtInventoryNumber.Location = new System.Drawing.Point(149, 21);
+            this.txtInventoryNumber.Name = "txtInventoryNumber";
+            this.txtInventoryNumber.Size = new System.Drawing.Size(99, 20);
+            this.txtInventoryNumber.TabIndex = 4;
+            // 
+            // txtShortName
+            // 
+            this.txtShortName.Location = new System.Drawing.Point(254, 21);
+            this.txtShortName.Name = "txtShortName";
+            this.txtShortName.Size = new System.Drawing.Size(91, 20);
+            this.txtShortName.TabIndex = 5;
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(351, 21);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(100, 20);
+            this.txtFullName.TabIndex = 6;
+            // 
+            // txtProfessionalLevel
+            // 
+            this.txtProfessionalLevel.Location = new System.Drawing.Point(457, 21);
+            this.txtProfessionalLevel.Name = "txtProfessionalLevel";
+            this.txtProfessionalLevel.Size = new System.Drawing.Size(91, 20);
+            this.txtProfessionalLevel.TabIndex = 7;
+            // 
+            // txtWearPercentage
+            // 
+            this.txtWearPercentage.Location = new System.Drawing.Point(554, 21);
+            this.txtWearPercentage.Name = "txtWearPercentage";
+            this.txtWearPercentage.Size = new System.Drawing.Size(91, 20);
+            this.txtWearPercentage.TabIndex = 8;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(651, 21);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantity.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 609);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtWearPercentage);
+            this.Controls.Add(this.txtProfessionalLevel);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.txtShortName);
+            this.Controls.Add(this.txtInventoryNumber);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtInventoryNumber;
+        private System.Windows.Forms.TextBox txtShortName;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox txtProfessionalLevel;
+        private System.Windows.Forms.TextBox txtWearPercentage;
+        private System.Windows.Forms.TextBox txtQuantity;
+    }
+}
+
---
+++ ManageToolApp/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManageToolApp
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=BDSQL.db;";
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var query = "SELECT * FROM tool"; 
+                var adapter = new SQLiteDataAdapter(query, connection);
+                var dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void AddTool(string inventoryNumber, string shortName, string fullName, int professionalLevel, int wearPercentage, int quantity)
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var query = "INSERT INTO tool (inventory_number, short_name, full_name, professional_level, wear_percentage, quantity) " +
+                            "VALUES (@inventoryNumber, @shortName, @fullName, @professionalLevel, @wearPercentage, @quantity)";
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    // Параметризованный запрос
+                    command.Parameters.AddWithValue("@inventoryNumber", inventoryNumber);
+                    command.Parameters.AddWithValue("@shortName", shortName);
+                    command.Parameters.AddWithValue("@fullName", fullName);
+                    command.Parameters.AddWithValue("@professionalLevel", professionalLevel);
+                    command.Parameters.AddWithValue("@wearPercentage", wearPercentage);
+                    command.Parameters.AddWithValue("@quantity", quantity);
+
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Инструмент успешно добавлен.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка добавления инструмента: " + ex.Message);
+                    }
+                }
+            }
+            // Обновляем таблицу с данными
+            LoadData();
+        }
+
+        private void UpdateTool(int toolId, string inventoryNumber, string shortName, string fullName, int professionalLevel, int wearPercentage, int quantity)
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var query = "UPDATE tool SET inventory_number = @inventoryNumber, short_name = @shortName, full_name = @fullName, professional_level = @professionalLevel, wear_percentage = @wearPercentage, quantity = @quantity WHERE tool_id = @toolId";
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@toolId", toolId);
+                    command.Parameters.AddWithValue("@inventoryNumber", inventoryNumber);
+                    command.Parameters.AddWithValue("@shortName", shortName);
+                    command.Parameters.AddWithValue("@fullName", fullName);
+                    command.Parameters.AddWithValue("@professionalLevel", professionalLevel);
+                    command.Parameters.AddWithValue("@wearPercentage", wearPercentage);
+                    command.Parameters.AddWithValue("@quantity", quantity);
+                    command.ExecuteNonQuery();
+                }
+            }
+            LoadData();
+        }
+
+        private void DeleteTool(int toolId)
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var query = "DELETE FROM tool WHERE tool_id = @toolId";
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@toolId", toolId);
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Инструмент удален.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Инструмент не найден.");
+                    }
+                }
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Собираем данные из текстовых полей
+            string inventoryNumber = txtInventoryNumber.Text;
+            string shortName = txtShortName.Text;
+            string fullName = txtFullName.Text;
+            int professionalLevel;
+            int wearPercentage;
+            int quantity;
+
+            // Проверяем, корректны ли введенные данные
+            if (string.IsNullOrWhiteSpace(inventoryNumber) ||
+                string.IsNullOrWhiteSpace(shortName) ||
+                string.IsNullOrWhiteSpace(fullName) ||
+                !int.TryParse(txtProfessionalLevel.Text, out professionalLevel) ||
+                !int.TryParse(txtWearPercentage.Text, out wearPercentage) ||
+                !int.TryParse(txtQuantity.Text, out quantity))
+            {
+                MessageBox.Show("Заполните все данные.");
+                return;
+            }
+
+            // Добавляем инструмент в базу данных
+            AddTool(inventoryNumber, shortName, fullName, professionalLevel, wearPercentage, quantity);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateTool(1, "INV-001", "Updated Hammer", "Updated Professional Hammer", 2, 20, 60);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+                var toolId = Convert.ToInt32(selectedRow.Cells["tool_id"].Value);
+
+                DeleteTool(toolId);
+            }
+            else
+            {
+                MessageBox.Show("Выберите строку для удаления.");
+            }
+        }
+    }
+}
